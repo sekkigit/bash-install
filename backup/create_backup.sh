948df904b434a5ce1f234c5f1127d00f3c706530
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-07-10 17:19:26 +0200

    1

--- backup/create_backup.sh
+++ backup/create_backup.sh
@@ -9,7 +9,7 @@ mkdir /backup-task
 cat <<EOF >> /backup-task/backup-daily.sh
 #!/bin/bash
 
-tar -zcf /backup/daily/backup-\$(date +%Y%m%d).tar.gz -C $SOURCE/*
+tar -zcf /backup/daily/backup-\$(date +%Y%m%d).tar.gz -C ${SOURCE}/*
 find /backup/daily/* -mtime +7 -delete -exec rm {} \; 
 EOF
 chmod +x /backup-task/backup-daily.sh
@@ -17,7 +17,7 @@ chmod +x /backup-task/backup-daily.sh
 cat <<EOF >> /backup-task/backup-weekly.sh
 #!/bin/bash
 
-tar -zcf /backup/weekly/backup-\$(date +%Y%m%d).tar.gz -C $SOURCE/*
+tar -zcf /backup/weekly/backup-\$(date +%Y%m%d).tar.gz -C ${SOURCE}/*
 find /backup/weekly/* -mtime +31 -delete -exec rm {} \; 
 EOF
 chmod +x /backup-task/backup-weekly.sh
@@ -25,7 +25,7 @@ chmod +x /backup-task/backup-weekly.sh
 cat <<EOF >> /backup-task/backup-monthly.sh
 #!/bin/bash
 
-tar -zcf /backup/weekly/backup-\$(date +%Y%m%d).tar.gz -C $SOURCE/*
+tar -zcf /backup/weekly/backup-\$(date +%Y%m%d).tar.gz -C ${SOURCE}/*
 find /backup/monthly/* -mtime +365 -delete -exec rm {} \; 
 EOF
 chmod +x /backup-task/backup-monthly.sh
@@ -36,4 +36,3 @@ cat <<EOF >> /etc/cron.d/crontask
 50 5 1 * * root    /backup-task/backup-monthly.sh
 EOF
 crontab -u $(who am i | awk '{print $1}') /etc/cron.d/crontask
-sudo bash /backup-task/backup-daily.sh
