6b03c2a21650c7aabe0d8b0d45b6b22b11901b96
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-07-10 15:28:36 +0200

    1

--- backup/create_backup.sh
+++ backup/create_backup.sh
@@ -4,28 +4,28 @@ source .var
 
 mkdir /backup
 mkdir /backup/{daily,weekly,monthly}
-mkdir /home/"$USER"/backup-task
+mkdir /home/"${USER}"/backup-task
 
-cat <<EOF >> /home/"$USER"/backup-task/backup-daily.sh
+cat <<EOF >> /home/"${USER}"/backup-task/backup-daily.sh
 #!/bin/bash
 
-tar -zcf \/backup/daily/backup-$(date +%Y%m%d).tar.gz\ -C $SOURCE/*
+tar -zcf /backup/daily/backup-'$(date +%Y%m%d)'.tar.gz -C $SOURCE/*
 find /home/$USER/backup/daily/* -mtime +7 -delete -exec rm {} \; 
 EOF
 chmod +x /home/"$USER"/backup-task/backup-daily.sh
 
-cat <<EOF >> /home/"$USER"/backup-task/backup-weekly.sh
+cat <<EOF >> /home/"${USER}"/backup-task/backup-weekly.sh
 #!/bin/bash
 
-tar -zcf \/backup/weekly/backup-$(date +%Y%m%d).tar.gz\ -C $SOURCE/*
+tar -zcf /backup/weekly/backup-'$(date +%Y%m%d)'.tar.gz -C $SOURCE/*
 find /home/$USER/backup/weekly/* -mtime +31 -delete -exec rm {} \; 
 EOF
 chmod +x /home/"$USER"/backup-task/backup-weekly.sh
 
-cat <<EOF >> /home/"$USER"/backup-task/backup-monthly.sh
+cat <<EOF >> /home/"${USER}"/backup-task/backup-monthly.sh
 #!/bin/bash
 
-tar -zcf \/backup/weekly/backup-$(date +%Y%m%d).tar.gz\ -C $SOURCE/*
+tar -zcf /backup/weekly/backup-'$(date +%Y%m%d)'.tar.gz -C $SOURCE/*
 find /home/$USER/backup/monthly/* -mtime +365 -delete -exec rm {} \; 
 EOF
 chmod +x /home/"$USER"/backup-task/backup-monthly.sh
