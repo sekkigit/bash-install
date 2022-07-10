2a27e0ecd97a9e2249f8b185c3dbc9dd3997f633
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-07-10 14:03:08 +0200

    1

--- backup/create_backup.sh
+++ backup/create_backup.sh
@@ -4,35 +4,35 @@ source .var
 
 mkdir /backup
 mkdir /backup/{daily,weekly,monthly}
-mkdir /home/"$USER"/backup-task
+mkdir /home/$USER/backup-task
 
 cat <<EOF >> /home/"$USER"/backup-task/backup-daily.sh
 #!/bin/bash
 
-tar -zcf /backup/daily/backup-$(date +%Y%m%d).tar.gz -C "$SOURCE"/*
-find /home/"$USER"/backup/daily/* -mtime +7 -delete -exec rm {} \; 
+tar -zcf /backup/daily/backup-$(date +%Y%m%d).tar.gz -C $SOURCE/*
+find /home/$USER/backup/daily/* -mtime +7 -delete -exec rm {} \; 
 EOF
-chmod +x /home/"$USER"/backup-task/backup-daily.sh
+chmod +x /home/$USER/backup-task/backup-daily.sh
 
-cat <<EOF >> /home/"$USER"/backup-task/backup-weekly.sh
+cat <<EOF >> /home/$USER/backup-task/backup-weekly.sh
 #!/bin/bash
 
-tar -zcf /backup/weekly/backup-$(date +%Y%m%d).tar.gz -C "$SOURCE"/*
-find /home/"$USER"/backup/weekly/* -mtime +31 -delete -exec rm {} \; 
+tar -zcf /backup/weekly/backup-$(date +%Y%m%d).tar.gz -C $SOURCE/*
+find /home/$USER/backup/weekly/* -mtime +31 -delete -exec rm {} \; 
 EOF
-chmod +x /home/"$USER"/backup-task/backup-weekly.sh
+chmod +x /home/$USER/backup-task/backup-weekly.sh
 
-cat <<EOF >> /home/"$USER"/backup-task/backup-monthly.sh
+cat <<EOF >> /home/$USER/backup-task/backup-monthly.sh
 #!/bin/bash
 
-tar -zcf /backup/weekly/backup-$(date +%Y%m%d).tar.gz -C "$SOURCE"/*
-find /home/"$USER"/backup/monthly/* -mtime +365 -delete -exec rm {} \; 
+tar -zcf /backup/weekly/backup-$(date +%Y%m%d).tar.gz -C $SOURCE/*
+find /home/$USER/backup/monthly/* -mtime +365 -delete -exec rm {} \; 
 EOF
-chmod +x /home/"$USER"/backup-task/backup-monthly.sh
+chmod +x /home/$USER/backup-task/backup-monthly.sh
 
 cat <<EOF >> /etc/cron.d/crontask
-30 5 * * * root    /home/"$USER"/backup-task/backup-daily.sh
-40 5 * * 1 root    /home/"$USER"/backup-task/backup-weekly.sh
-50 5 1 * * root    /home/"$USER"/backup-task/backup-monthly.sh
+30 5 * * * root    /home/$USER/backup-task/backup-daily.sh
+40 5 * * 1 root    /home/$USER/backup-task/backup-weekly.sh
+50 5 1 * * root    /home/$USER/backup-task/backup-monthly.sh
 EOF
-crontab -u "$USER" /etc/cron.d/crontask
+crontab -u $USER /etc/cron.d/crontask
